223d99485fcd92c89eb10e77c01e877a01278f1b
Pedro Inácio Penha dos Santos <inaciopedro2004@gmail.com>
2023-10-01 17:06:44 -0300

    API

---
+++ app/main.js
@@ -0,0 +1,9 @@
+buscarProdutosAPI()
+
+async function buscarProdutosAPI() {
+    const dados = await fetch('https://pedroinaciop.github.io/zndental/dados.json')
+    const produtos = await dados.json()
+    console.table(produtos)
+    exibirLivrosNaTela(produtos)
+}
+
